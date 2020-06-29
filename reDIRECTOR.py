e37b99d23413ada2c69f73e98df2a72e53cfdc9a
Mark Hargreaves <clashclanacc2602@gmail.com>
2020-06-29 15:11:59 +0300

    update

--- reDIRECTOR.py
+++ reDIRECTOR.py
@@ -3,7 +3,6 @@ from matrix_client.api import MatrixHttpApi
 from matrix_client.client import MatrixClient
 from matrix_client.room import Room
 
-
 class Director:
 	def __init__(self):
 		self.BOTUSERNAME = "pmos_redirector_bot"
@@ -15,22 +14,20 @@ class Director:
 		self.token = self.MainClient.login_with_password(username=self.BOTUSERNAME, password=self.BOTPASSWORD)
 		self.APIWrapper = MatrixHttpApi("https://"+self.BOTSERVO, token=self.token)
 		self.target_room = Room(self.MainClient, self.RID)
-	def callback_event(room, event):
-		print(event)
-		if event.get("content").get("membership") == "join":
-			print("somewho joined")
-			user_id = event.get("sender")
-			print(user_id)
-			d.target_room.send_notice("This room is not official postmarketOS room. Please join the #porting:postmarketos.org room")
-			d.APIWrapper.invite_user(d.realRID, user_id)
+		print("ready")
 	def mainThread(self):
-		Users = self.target_room.get_joined_members()
 		self.MainClient.add_listener(self.callback_event, event_type="m.room.member")
 		self.MainClient.start_listener_thread()
-		for user in Users:
-			print(user.get_display_name())
 		while True:
 			input()
+	def callback_event(room, event):
+		if event.get("content").get("membership") == "join":
+			user_id = event.get("sender")
+			d.target_room.send_html('This room is not official postmarketOS room. Please join the <a href="https://matrix.to/#/#porting:postmarketos.org">#porting:postmarketos.org</a> room')
+			try:
+				d.APIWrapper.invite_user(d.realRID, user_id)
+			except:
+				pass # user already joined the rooms
 if __name__ == "__main__":
 	d = Director()
 	d.mainThread()
